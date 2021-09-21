0db76e6a70f50f7b8b64771d5b2e74130b69aa32
Ankit Singh Rathore <asinghrathore0@gmail.com>
2021-09-21 15:14:53 +0530

    [Ankit] Add: Welcome Message

---
+++ EmployeePayroll_MultiThreading/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayroll_MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MultiThreading Employee Payroll");
+        }
+    }
+}
